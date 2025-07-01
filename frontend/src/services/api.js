e64d52512c1e54b1271261c4810925dfb534cca1
Alchemist70 <herberzs70@gmail.com>
2025-07-01 07:54:49 +0530

    New commit

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,18 +1,18 @@
-import axios from 'axios';
+import axios from "axios";
 
-const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const baseURL = process.env.REACT_APP_API_URL || "/api";
 
 export const api = axios.create({
   baseURL,
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 // Add a request interceptor to add the auth token to requests
 api.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -21,4 +21,4 @@ api.interceptors.request.use(
   (error) => {
     return Promise.reject(error);
   }
-); 
+);
